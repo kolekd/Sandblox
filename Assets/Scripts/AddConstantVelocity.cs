88e0b02bcb051f771f54f4a7d204d492f839d571
Daniel <kolekd007@gmail.com>
2019-02-20 17:24:04 +0100

    Tree that can fall appart

--- Assets/Scripts/AddConstantVelocity.cs
+++ Assets/Scripts/AddConstantVelocity.cs
@@ -7,7 +7,7 @@ public class AddConstantVelocity : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().velocity += new Vector3(0, 0, 1);
+        GetComponent<Rigidbody>().velocity += new Vector3(0, 0, 5);
     }
 
     // Update is called once per frame
--- Assets/Scripts/BreakOnCollision.cs
+++ Assets/Scripts/BreakOnCollision.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class BreakOnCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    string strTag;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.collider.tag == strTag)
+        {
+            //  Destroy(GetComponent<Rigidbody>());
+            //            GetComponent<Rigidbody>().velocity += new Vector3(0 , 0, 20);
+
+
+            //            for(int i = 0; i < collision.contactCount; i++)
+            //            {
+            //                Renderer renderer = collision.contacts[0].thisCollider.GetComponent<Renderer>();
+            //                renderer.material.shader = Shader.Find("_Color");
+            //                renderer.material.SetColor("_Color", Color.cyan);
+            // CHILD OF PARENT CHANGES ON CONTACT
+            //            }
+
+            Destroy(GetComponent<Rigidbody>());
+
+            Component[] components = GetComponentsInChildren<Component>();
+
+            foreach(Component value in components)
+            {
+                value.gameObject.AddComponent<Rigidbody>();
+            }
+
+//            Renderer[] renderer = GetComponentsInChildren<Renderer>();
+//                       
+//            foreach(Renderer value in renderer)
+//            {
+//                value.material.shader = Shader.Find("_Color");
+//                value.material.SetColor("_Color", Color.cyan);
+//            }
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
