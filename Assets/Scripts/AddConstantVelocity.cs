2a350ded5751d8beb40057b77825befafafc61a7
Daniel <kolekd007@gmail.com>
2019-02-20 15:53:10 +0100

    working on loose trees

---
+++ Assets/Scripts/AddConstantVelocity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddConstantVelocity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity += new Vector3(0, 0, 1);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    }
+}
