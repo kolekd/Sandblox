fe7cf3f23c9635f0b50cdc084670154b8afaa018
Marek Wimmer <marek.wimmer@gmail.com>
2019-02-20 16:54:44 +0100

    jumping implemented

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -3,6 +3,7 @@
 public class playerController : MonoBehaviour{
 
     public float speed = 10.0F;
+    bool onGround = true;
 
     // Start is called before the first frame update
     void Start(){
@@ -19,6 +20,26 @@ public class playerController : MonoBehaviour{
         straffe *= Time.deltaTime;
 
         transform.Translate(straffe, 0, forwards);
+
+        RaycastHit hit;
+        Vector3 jumpCentre = this.transform.position + this.GetComponent<BoxCollider>().center;
+
+        if(Physics.Raycast(jumpCentre, Vector3.down, out hit, 0.5F))
+        {
+            if(hit.transform.gameObject.tag != "Player")
+            {
+                onGround = true;
+            }
+        }
+        else
+        {
+            onGround = false;
+        }
+        Debug.Log(onGround);
+
+        //jumping
+        if (Input.GetKeyDown("space") && onGround)
+            this.GetComponent<Rigidbody>().AddForce(Vector3.up * 200);
 
         //unlock cursor ingame
         if (Input.GetKeyDown("escape"))
